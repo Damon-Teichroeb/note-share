4a29c0942315bae6dde8ac35d21d458d49c94337
Damon-Teichroeb <75189960+Damon-Teichroeb@users.noreply.github.com>
2021-06-25 22:35:52 -0700

    Add files via upload

--- note_share/index.php
+++ note_share/index.php
@@ -5,15 +5,15 @@ require 'header.php';
 <section class="features">
   <div>
     <p>Browse our selection of notes from various majors and classes!</p>
-    <a class="btn" href="browse.php">Browse</a>
+    <div><a class="btn" href="browse.php">Browse</a></div>
   </div>
   <div>
     <p>Upload your notes for your peers to see!</p>
-    <a class="btn" href="upload.php">Upload</a>
+    <div><a class="btn" href="upload.php">Upload</a></div>
   </div>
   <div>
     <p>Don't have an account with us yet? signing up is easy!</p>
-    <a class="btn" href="signup.php">Signup Now!</a>
+    <div><a class="btn" href="signup.php">Signup Now!</a></div>
   </div>
 </section>
 
