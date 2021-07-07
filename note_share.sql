45aec2aacd68cdf2e0873e8d21df3bd3ed7ee79d
Damon-Teichroeb <75189960+Damon-Teichroeb@users.noreply.github.com>
2021-07-07 09:25:09 -0700

    Add files via upload

---
+++ note_share.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 07, 2021 at 06:24 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `note_share`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `favorites`
+--
+
+CREATE TABLE `favorites` (
+  `users_id` int(10) UNSIGNED NOT NULL,
+  `notes_id` int(10) UNSIGNED NOT NULL,
+  `favorites_id` int(20) UNSIGNED NOT NULL,
+  `favorites_liked` tinyint(1) NOT NULL DEFAULT 0,
+  `favorites_disliked` tinyint(1) NOT NULL DEFAULT 0,
+  `favorites_favorited` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notes`
+--
+
+CREATE TABLE `notes` (
+  `users_id` int(10) UNSIGNED NOT NULL,
+  `notes_id` int(10) UNSIGNED NOT NULL,
+  `notes_name` varchar(50) NOT NULL,
+  `notes_course` varchar(5) NOT NULL,
+  `notes_teacher` varchar(50) NOT NULL,
+  `notes_year` int(4) NOT NULL,
+  `notes_season` tinyint(1) UNSIGNED NOT NULL,
+  `notes_likes` int(20) DEFAULT NULL,
+  `notes_dislikes` int(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `notes`
+--
+
+INSERT INTO `notes` (`users_id`, `notes_id`, `notes_name`, `notes_course`, `notes_teacher`, `notes_year`, `notes_season`, `notes_likes`, `notes_dislikes`) VALUES
+(1, 1, 'SE2 Chapter 14', 'CS356', 'Dr. Sparks', 2021, 1, NULL, NULL),
+(1, 2, 'SE2 Chapter 15', 'CS356', 'Dr. Sparks', 2021, 1, NULL, NULL),
+(1, 3, 'SE2 Chapter 16', 'CS356', 'Dr. Sparks', 2021, 1, NULL, NULL),
+(1, 4, 'SE2 Chapter 17', 'CS356', 'Dr. Sparks', 2021, 1, NULL, NULL),
+(1, 5, 'SE2 Chapter 19', 'CS356', 'Dr. Sparks', 2021, 1, NULL, NULL),
+(1, 6, 'SE2 Chapter 20', 'CS356', 'Dr. Sparks', 2021, 1, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `users_id` int(10) UNSIGNED NOT NULL,
+  `users_email` varchar(50) NOT NULL,
+  `users_name` varchar(50) NOT NULL,
+  `users_password` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`users_id`, `users_email`, `users_name`, `users_password`) VALUES
+(1, 'hotwheeljets@hotmail.com', 'Damon', '$2y$10$5frDtIgAvYkP/ZGkGwW1d.ifzzG6GZ8Go.A8tRFXkz60lW1tXdo9.'),
+(2, 'test@hotmail.com', 'Test', '$2y$10$WhEQ9/95XBJJHq7rucoTcOO8yIEp7zREcDKp0opsGJuxfUvtHf2ni');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `favorites`
+--
+ALTER TABLE `favorites`
+  ADD PRIMARY KEY (`favorites_id`),
+  ADD UNIQUE KEY `users_id` (`users_id`,`notes_id`);
+
+--
+-- Indexes for table `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`notes_id`),
+  ADD UNIQUE KEY `users_id` (`users_id`,`notes_name`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`users_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `favorites`
+--
+ALTER TABLE `favorites`
+  MODIFY `favorites_id` int(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `notes_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `users_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
