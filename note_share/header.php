477c6f1ca839394d69b31a483819926e6953aee4
Damon-Teichroeb <75189960+Damon-Teichroeb@users.noreply.github.com>
2021-07-09 18:51:03 -0700

    Add files via upload

--- note_share/header.php
+++ note_share/header.php
@@ -19,46 +19,46 @@ else
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body onload="listNotes('');"> <!-- This onload is for browse-notes.php -->
+  <section class="flex-nav">
+    <?php
+    if (isset($login))
+    {
+      $object = $dbh->query("SELECT users_name FROM users WHERE users_id = '$login';");
+      $names  = $object->fetch_assoc();
+      ?>  
+      <a class="nav-btn" href="includes/logout.inc.php"><?php echo $names['users_name'];?>: Logout</a>
+      <a class="nav-btn" href="upload.php?">Upload</a>
+      <?php
+    }
+    else
+    {
+      ?>
+      <a class="nav-btn" href="login.php">Login</a>
+      <a class="nav-btn" href="signup.php">Register</a>
+      <?php
+    }
+    ?>
+    <a class="nav-btn" href="index.php">Home</a>
+    <?php
+    // Signup notifications
+    if (isset($_GET['signup']))
+      switch ($_GET['signup'])
+      {
+        case 'success':
+          echo "<p class=\"success\">You have successfully registered an account!</p>";
+      }
+    // Login notifications
+    if (isset($_GET['login']))
+      switch ($_GET['login'])
+      {
+        case 'success':
+          echo "<p class=\"success\">You have successfully logged into your account!</p>";
+          break;
+        case 'logout':
+          echo "<p class=\"success\">You have successfully logged out of your account!</p>";
+      }
+    ?>
+  </section>
   <header>
-    <h1><a href="index.php">Note Share</a></h1>
-    <section class="flex-nav">
-      <?php
-      if (isset($login))
-      {
-        $object = $dbh->query("SELECT users_name FROM users WHERE users_id = '$login';");
-        $names  = $object->fetch_assoc();
-        ?>  
-        <a class="nav-btn" href="includes/logout.inc.php"><?php echo $names['users_name'];?>: Logout</a>
-        <a class="nav-btn" href="upload.php?">Upload</a>
-        <?php
-      }
-      else
-      {
-        ?>
-        <a class="nav-btn" href="login.php">Login</a>
-        <a class="nav-btn" href="signup.php">Register</a>
-        <?php
-      }
-      ?>
-      <a class="nav-btn" href="index.php">Home</a>
-      <?php
-      // Signup notifications
-      if (isset($_GET['signup']))
-        switch ($_GET['signup'])
-        {
-          case 'success':
-            echo "<p class=\"success\">You have successfully registered an account!</p>";
-        }
-      // Login notifications
-      if (isset($_GET['login']))
-        switch ($_GET['login'])
-        {
-          case 'success':
-            echo "<p class=\"success\">You have successfully logged into your account!</p>";
-            break;
-          case 'logout':
-            echo "<p class=\"success\">You have successfully logged out of your account!</p>";
-        }
-      ?>
-    </section>
+    <h1>Welcome to Note Share!</h1>
   </header>
